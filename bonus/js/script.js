3f754e6349db5df5e7cac0e7b3f7c788dd472581
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-09-18 18:25:16 +0200

    feat: bonus ex (WIP)

---
+++ bonus/js/script.js
@@ -0,0 +1,60 @@
+// Esercizi:
+
+// 1️⃣
+// Chiedi due numeri all’utente e poi chiedi se vuole "somma" o "differenza".
+// Stampa il risultato corrispondente.
+
+// let numberOneStr = prompt("Dimmi un numero");
+// let numberTwoStr = prompt("Dimmi un secondo numero");
+
+// let math = prompt("Vuoi effettuare la somma o la differenza?");
+
+// let numberOne = parseInt(numberOneStr);
+// let numberTwo = parseInt(numberTwoStr);
+
+// result = 'Devi digitare "somma" o "differenza", tutto in minuscolo';
+
+// if (math === "somma") {
+//     result = numberOne + numberTwo
+// } else if (math === "differenza") {
+//     result = numberOne - numberTwo
+// }
+
+// console.log(result);
+
+
+// 2️⃣
+// Chiedi all'utente la sua età e comunichiamo l'anno di nascita.
+
+// 3️⃣
+// Chiedi all'utente il suo nome e stampa in console la stringa "ciao <nome_utente>".
+
+// let userName = prompt("Qual è il tuo nome?");
+// console.log(`Ciao ${userName}`);
+
+
+// 4️⃣
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+// let userName = prompt("Qual è il tuo nome?");
+// let userLastName = prompt("Qual è il tuo cognome?");
+// let userColor = prompt("Qual è il tuo colore preferito?");
+// console.log(userName + userLastName + userColor);
+
+
+// 5️⃣ 
+// Scrivi un programma che simula l’acquisto al minimarket.
+
+// Chiedi all’utente quale prodotto vuole acquistare:
+// "pane" → €1.50
+// "latte" → €1.20
+// "uova" → €2.60
+
+// Chiedi quante unità vuole acquistare.
+
+// Chiedi se possiede la tessera fedeltà ("si" o "no"). Se sì calcola lo sconto in base a queste regole:
+// Totale < 20€ → sconto 5%
+// Totale ≥ 20€ → sconto 10%
+
+// Stampa in console uno scontrino
